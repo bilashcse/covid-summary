b7af45135ddf44fd2accac927b39756db1f89750
Md Nazmul Hossain <bilashcse@gmail.com>
2020-05-20 03:09:19 +0600

    options added, help added, specific country summary added

--- cli.js
+++ cli.js
@@ -1,10 +1,13 @@
 #! /usr/bin/env node
 const axios = require("axios");
+const yargs = require("yargs");
+
 Spinner = require("cli-spinner").Spinner;
 
 const urlMap = {
   baseUrl: "https://api.covid19api.com",
-  summary: "/world/total"
+  summary: "/world/total",
+  country: `/total/dayone/country`
 };
 
 const spinnerObj = new Spinner({
@@ -28,6 +31,7 @@ const getCovidSummary = async () => {
 
     spinnerObj.stop(true);
     if (result) {
+      console.log("Covid-19 live world summary: ");
       console.log(result.data);
     }
   } catch (err) {
@@ -36,4 +40,63 @@ const getCovidSummary = async () => {
   }
 };
 
-getCovidSummary();
+const getSpecificCountrySummary = async name => {
+  try {
+    spinnerObj.start();
+    const url = `${urlMap.baseUrl}${urlMap.country}/${name}`;
+    const result = await axios({
+      method: "get",
+      url,
+      headers: { Accept: "application/json" }
+    });
+
+    spinnerObj.stop(true);
+    if (result) {
+      const data = result.data;
+      const lastIndex = result.data.length - 1;
+      console.log(`Covid-19 ${name} summary:`);
+      console.log({
+        "Total Confirmed": data[lastIndex].Confirmed,
+        "Total Death": data[lastIndex].Deaths,
+        "Total Recovered": data[lastIndex].Recovered,
+        "Total Active": data[lastIndex].Active
+      });
+    }
+  } catch (err) {
+    spinnerObj.stop(true);
+    console.error(err);
+  }
+}
+
+async function init() {
+  const yargs = require("yargs");
+
+  const argv = yargs
+    .command("live", "Show live covid-19 summary", {
+      country: {
+        alias: "c",
+        description: "Specific country to show summary",
+        type: "string"
+      }
+    })
+    .option("country", {
+      alias: "c",
+      description: "Specific country to show summary",
+      type: "string"
+    })
+    .help()
+    .alias("help", "h").argv;
+
+  if (argv._.includes("live")) {
+    const country = argv.country || null;
+    if (country) {
+      await getSpecificCountrySummary(country);
+    } else {
+      await getCovidSummary();
+    }
+  }
+}
+
+(function() {
+  init();
+})();
